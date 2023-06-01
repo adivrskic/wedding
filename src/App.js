c68a6414b918890905e33734f4a53f89b0ff352c
Adi Vrskic <adivrskic123@gmail.com>
2023-06-01 16:59:44 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
+import { IoCloseCircleOutline } from "react-icons/io5";
 function App() {
+  const [selectedVideo, setSelectedVideo] = useState("");
+
+  //https://youtu.be/4FOV-G4fDjU
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="app">
+      <main className="main">
+        <h1>Dajla & Adi</h1>
+        <div
+          className="wedding-highlights"
+          onClick={() =>
+            setSelectedVideo(
+              "https://youtube.com/embed/4FOV-G4fDjU?controls=0&rel=0&showinfo=0&modestbranding=1&allowfullscreen=1"
+            )
+          }
         >
-          Learn React
-        </a>
-      </header>
+          <p>
+            Svadba
+            <br />
+            Highlights
+          </p>
+          <img src="/assets/images/highlights.png" alt="wedding highlights" />
+        </div>
+        <div
+          className="wedding-full"
+          onClick={() =>
+            setSelectedVideo(
+              "https://player.vimeo.com/video/832386284?h=0cba7347e0&autoplay=1&controls=0"
+            )
+          }
+        >
+          <p>
+            Svadba
+            <br />
+            Full
+          </p>
+          <img src="/assets/images/full.png" alt="wedding full" />
+        </div>
+
+        {selectedVideo && (
+          <div className="video">
+            <iframe
+              width="100%"
+              height="100%"
+              src={selectedVideo}
+              autoplay
+              preload="auto"
+            />
+            <button className="video-close">
+              <IoCloseCircleOutline onClick={() => setSelectedVideo("")} />
+            </button>
+          </div>
+        )}
+      </main>
     </div>
   );
 }
